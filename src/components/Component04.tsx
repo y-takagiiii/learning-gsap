6975d9e1bb88d903e9f3bf524ef9adb92302b7fc
y-takagiiii <takagiyuri0712@gmail.com>
2024-04-20 10:51:14 +0900

    一時保存

--- src/components/Component04.tsx
+++ src/components/Component04.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import styles from "./Component04.module.css";
+
+gsap.registerPlugin(useGSAP);
 
 const Component04 = () => {
-  return (
-    <div>Component04</div>
-  )
-}
+  const container = useRef<HTMLDivElement>(null);
+  const tl = useRef<gsap.core.Timeline | null>(null);
 
-export default Component04
+  useGSAP(
+    () => {
+      tl.current = gsap
+        .timeline()
+        .to(`.${styles.box}`, {
+          rotate: 360,
+        })
+        .to(`.${styles.circle}`, { x: 100 });
+    },
+    { scope: container }
+  );
+
+  return (
+    <div className={styles.container} ref={container}>
+      <div className={styles.box}>box</div>
+      <div className={styles.circle}>circle</div>
+    </div>
+  );
+};
+
+export default Component04;
--- src/components/Component05.tsx
+++ src/components/Component05.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import styles from "./Component05.module.css";
+
+gsap.registerPlugin(useGSAP);
 
 const Component05 = () => {
-  return <div>Component05</div>;
+  const container = useRef<HTMLDivElement>(null);
+  const tl = useRef<gsap.core.Timeline | null>(null);
+  const { contextSafe } = useGSAP(
+    () => {
+      console.log("creating timeline");
+      tl.current = gsap
+        .timeline()
+        .to(`.${styles.box}`, {
+          rotation: 360,
+        })
+        .to(`.${styles.circle}`, {
+          x: 100,
+        });
+    },
+    { scope: container }
+  );
+  const handleToggle = contextSafe(() => {
+    if (tl.current) {
+      tl.current.reversed(!tl.current.reversed());
+    }
+  });
+
+  return (
+    <div className={styles.container} ref={container}>
+        <button className={styles.button} onClick={handleToggle}>
+          TOGGLE
+        </button>
+        <div className={styles.box}>box</div>
+        <div className={styles.circle}>circle</div>
+    </div>
+  );
 };
 
 export default Component05;
